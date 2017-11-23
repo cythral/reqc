ab2756aba7ba8ab7eb8180b49f3af1d4fcef79c2
Cythral <talen@cythral.com>
2017-11-23 13:09:43 -0600

    adjust constants path in test

--- build/constantsHttpTest.php
+++ build/constantsHttpTest.php
@@ -1,5 +1,5 @@
 <?php
 
-include dirname(__DIR__)."/constants.php";
+include dirname(__DIR__)."/src/constants.php";
 header("Content-Type: application/json");
 die(json_encode(["HOST" => reqc\HOST]));
