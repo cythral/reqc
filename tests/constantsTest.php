43add7b3e7d6d5b74e8ace70a8c2259d345b1777
Cythral <talen@cythral.com>
2017-11-24 12:33:19 -0600

    add http constants localhost sd1 test

--- tests/constantsTest.php
+++ tests/constantsTest.php
@@ -62,4 +62,34 @@ class constantsTest extends TestCase {
 		$this->assertFalse(isset($constants["reqc\\DOMAIN"]));
 		$this->assertEquals("localhost", $constants["reqc\\TLD"]);
 	}
+
+	public function testHttpConstantsLocalhostSd1() {
+		$req = new reqc\request([
+			"url" => "http://sd1.localhost/build/constantsHttpTest.php",
+			"method" => "GET"
+		]);
+
+		// make sure we got a valid response
+		$this->assertEquals(200, $req->response->code);
+		$constants = json_decode($req->response->body, true);
+
+		$this->assertEquals(["*/*"], $constants["reqc\\ACCEPT"]);
+		$this->assertEquals(80, $constants["reqc\\PORT"]);
+		$this->assertEquals([], $constants["reqc\\VARS"]);
+		$this->assertEquals("sd1.localhost", $constants["reqc\\HOST"]);
+		$this->assertEquals("GET", $constants["reqc\\METHOD"]);
+		$this->assertEquals("sd1.localhost", $constants["reqc\\BASEURL"]);
+		$this->assertEquals("http://sd1.localhost/build/constantsHttpTest.php", $constants["reqc\\FULLURL"]);
+		$this->assertEquals("/build/constantsHttpTest.php", $constants["reqc\\URI"]);
+		$this->assertEquals(2, $constants["reqc\\SUBTYPE"]);
+		$this->assertEquals("/build", $constants["reqc\\DIRECTORY"]);
+		$this->assertEquals("constantsHttpTest", $constants["reqc\\FILENAME"]);
+		$this->assertEquals("php", $constants["reqc\\EXTENSION"]);
+		$this->assertEquals("constantsHttpTest.php", $constants["reqc\\FILE"]);
+		$this->assertEquals("/build/constantsHttpTest.php", $constants["reqc\\PATH"]);
+		$this->assertEquals(false, $constants["reqc\\H2PUSH"]);
+		$this->assertFalse(isset($constants["reqc\\SUBDOMAIN"]));
+		$this->assertEquals("sd1", $constants["reqc\\DOMAIN"]);
+		$this->assertEquals("localhost", $constants["reqc\\TLD"]);
+	}
 }
